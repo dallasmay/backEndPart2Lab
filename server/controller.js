14cbe4a6ef11da015b9d079a4fe7e190b55aba98
dallasmay <dmmay9@gmail.com>
2022-06-14 17:07:22 -0600

    completed lab

--- server/controller.js
+++ server/controller.js
@@ -7,7 +7,7 @@ module.exports = {
         res.status(200).send(houses);
     },
     deleteHouse: (req, res) => {
-        let index = houses.findIndex((element) => element.id === req.params.id);
+        let index = houses.findIndex((element) => element.id === +req.params.id);
         houses.splice(index, 1);
         res.status(200).send(houses);
     },
@@ -15,14 +15,23 @@ module.exports = {
         const {address, price, imageURL} = req.body;
         let newHouse = {
             address: address,
-            price: price,
+            price: +price,
             imageURL: imageURL,
             id: globalID
         };
         houses.push(newHouse);
         res.status(200).send(houses);
+        globalID++;
     },
     updateHouse: (req, res) => {
-        
+        let index = houses.findIndex((element) => element.id === +req.params.id);
+        let type = req.body.type;
+        if (type === "plus") {
+            houses[index].price += 10000;
+            res.status(200).send(houses);
+        } else if (type === "minus" && houses[index].price > 9999) {
+            houses[index].price -= 10000;
+            res.status(200).send(houses);
+        }
     }
 };
