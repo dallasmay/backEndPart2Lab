dc601863b39dd3202be8b73e050236ea1a32ef89
dallasmay <dmmay9@gmail.com>
2022-06-14 14:40:04 -0600

    completed part two: setting up endpoints

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,6 @@
 module.exports = {
-    getHouses:
-    deleteHouse:
-    createHouse:
-    updateHouse:
+    getHouses: "",
+    deleteHouse: "",
+    createHouse: "",
+    updateHouse: ""
 };
--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const {getHouses, createHouse, updateHouse, deleteHouse} = require("./controller");
+
+app.get("/api/houses", getHouses);
+app.post("/api/houses", createHouse);
+app.put("/api/houses/:id", updateHouse);
+app.delete("/api/houses/:id", deleteHouse);
 
 
 app.listen(4004, console.log("Running on port 4004"));
