ccd5ed9afbdf8997113682b34402e7275243b35b
dallasmay <dmmay9@gmail.com>
2022-06-14 15:00:40 -0600

    completed 1-5 on part 3

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,28 @@
+const houses = require("./db.json");
+
+let globalID = 4;
+
 module.exports = {
-    getHouses: "",
-    deleteHouse: "",
-    createHouse: "",
-    updateHouse: ""
+    getHouses: (req, res) => {
+        res.status(200).send(houses);
+    },
+    deleteHouse: (req, res) => {
+        let index = houses.findIndex((element) => element.id === req.params.id);
+        houses.splice(index, 1);
+        res.status(200).send(houses);
+    },
+    createHouse: (req, res) => {
+        const {address, price, imageURL} = req.body;
+        let newHouse = {
+            address: address,
+            price: price,
+            imageURL: imageURL,
+            id: globalID
+        };
+        houses.push(newHouse);
+        res.status(200).send(houses);
+    },
+    updateHouse: (req, res) => {
+        
+    }
 };
